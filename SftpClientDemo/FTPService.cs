e282b652fb4fe315aaeacffb32956d2096b0453b
zulqornain <zulqornain.gambari@stanbicibtc.com>
2020-08-20 02:42:20 +0100

    initialize repo

---
+++ SftpClientDemo/FTPService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WinSCP;
+
+namespace SftpClientDemo
+{
+    public class FTPService
+    {
+        public void SendFileThroughSFTP(string fulFilePath, string hostName, string userName, string password, string sshHostKeyFingerprint, string pathOnHost)
+        {
+            LoggerService logger = new LoggerService();
+            try
+            {
+                Console.WriteLine("Attempting to connect to SFTP session...");
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = hostName,
+                    UserName = userName,
+                    Password = password,
+                    PortNumber = 22,
+                    SshHostKeyFingerprint = sshHostKeyFingerprint
+                };
+
+                using Session session = new Session();
+                // Connect
+                session.Open(sessionOptions);
+
+                Console.WriteLine("Connection to SFTP session succeeded.");
+                // Upload files
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+                transferOptions.ResumeSupport.State = TransferResumeSupportState.Off;
+
+                TransferOperationResult transferResult;
+
+                //copy file to new location on host machine
+                if (File.Exists(fulFilePath))
+                {
+                    Console.WriteLine("Attempting to send file through SFTP...");
+                    string MTNTRNLOG_PushPath = pathOnHost;
+                    transferResult = session.PutFiles(fulFilePath, MTNTRNLOG_PushPath, false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+                    Console.WriteLine("File sent through SFTP successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("The file specified does not exist.");
+                }
+            }
+            catch (WinSCP.SessionException se)
+            {
+                Console.WriteLine($"Something went wrong:  {se.Message}\n");
+                Console.WriteLine("Please check the logs for the full exception.\n\n");
+                logger.LogError(se);
+                GC.Collect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong:  {ex.Message}\n");
+                Console.WriteLine("Please check the logs for the full exception.\n\n");
+                logger.LogError(ex);
+            }
+        }
+    }
+}
---
+++ SftpClientDemo/LoggerService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace SftpClientDemo
+{
+    public class LoggerService
+    {
+        private static object mutex;
+        string directory = ConfigurationManager.AppSettings["logPath"];
+        public LoggerService()
+        {
+            mutex = new object();
+        }
+
+        public void LogError(Exception exception)
+        {
+            try
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                string filepath = directory + @"\" + DateTime.Now.Date.ToString("dd-MMM-yyyy") + ".txt";
+                lock (mutex)
+                {
+                    File.AppendAllText(filepath, "Event Time: " + DateTime.Now.ToString() + " | Exception: " + exception + Environment.NewLine);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to write to log file: {ex.Message}\n");
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            try
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                string filepath = directory + @"\" + DateTime.Now.Date.ToString("dd-MMM-yyyy") + ".txt";
+                lock (mutex)
+                {
+                    File.AppendAllText(filepath, "Event Time: " + DateTime.Now.ToString() + " | Message: " + message + Environment.NewLine);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to write to log file: {ex.Message}\n");
+            }
+        }
+    }
+}
